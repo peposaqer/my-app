2d15b11f93be7f17b1b4b726b418c19a95d71f8d
Ahmed Hesham <ahmed.hesham@viewpluscorp>
2023-01-20 21:08:05 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
