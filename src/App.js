50885dea627bed7ba065c0488a881c5844bc4a26
Ahmed Hesham <ahmed.hesham@viewpluscorp>
2023-01-20 21:20:59 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.scss";
-import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
@@ -7,11 +7,11 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={window.location.pathname || ''}>
       <Navbar />
       <Routes>
-        <Route path="/" exact Component={Home}/>
-        <Route path="/About" exact Component={About}/>
+        <Route path="/" exact element={<Home />}/>
+        <Route path="/About" exact element={<About />}/>
       </Routes>
       <Footer />
     </Router>
